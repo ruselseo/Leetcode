b5c9dd17df8dd12058afd33896ddbac31dca178c
ruselseo <113802744+ruselseo@users.noreply.github.com>
2024-11-10 00:06:17 +0600

    Create ValidateSequence.java

---
+++ src/main/java/ValidateSequence.java
@@ -0,0 +1,36 @@
+package com.ruselseo.leetcode.easy;
+
+import java.util.List;
+
+/**
+ *
+ */
+
+public class ValidateSubsequence {
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int sequenceIndex = 0;
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).equals(sequence.get(sequenceIndex))) {
+                sequenceIndex++;
+            }
+            if (sequenceIndex == sequence.size()) return true;
+        }
+        return false;
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+        char[] first  = s.toCharArray();
+        char[] second = t.toCharArray();
+        int secondIndex = 0;
+        if (s.equals(t)) return true;
+        for (int i = 0; i < first.length; i++) {
+            if (second.length!=0 && first[i] == second[secondIndex]) {
+                secondIndex++;
+            }
+            if (secondIndex == second.length) return true;
+        }
+        return false;
+    }
+}

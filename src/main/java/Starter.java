4b86103e8bd200ba432802bedc545908a85d8f45
Ruslan Ibragimov <ruselseo@gmail.com>
2024-11-11 14:56:07 +0600

    Fixed TournamentWinner competitions cycle instead of linkedMap

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -2,22 +2,32 @@ package main.java;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Starter {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> competitions =
-                new ArrayList<ArrayList<String>>();
-        ArrayList<String> competition1 =
-                new ArrayList<String>(Arrays.asList("HTML", "C#"));
-        ArrayList<String> competition2 =
-                new ArrayList<String>(Arrays.asList("C#", "Python"));
-        ArrayList<String> competition3 =
-                new ArrayList<String>(Arrays.asList("Python", "HTML"));
-        competitions.add(competition1);
-        competitions.add(competition2);
-        competitions.add(competition3);
-        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(0, 0, 1));
+//        ArrayList<ArrayList<String>> competitions =
+//                new ArrayList<ArrayList<String>>();
+//        ArrayList<String> competition1 =
+//                new ArrayList<String>(Arrays.asList("HTML", "C#"));
+//        ArrayList<String> competition2 =
+//                new ArrayList<String>(Arrays.asList("C#", "Python"));
+//        ArrayList<String> competition3 =
+//                new ArrayList<String>(Arrays.asList("Python", "HTML"));
+//        competitions.add(competition1);
+//        competitions.add(competition2);
+//        competitions.add(competition3);
+//        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(0, 1, 1));
+        ArrayList<ArrayList<String>> competitions3 = new ArrayList<>(List.of(
+                new ArrayList<>(List.of("HTML", "Java")),
+                new ArrayList<>(List.of("Java", "Python")),
+                new ArrayList<>(List.of("Python", "HTML")),
+                new ArrayList<>(List.of("C#", "Python")),
+                new ArrayList<>(List.of("Java", "C#")),
+                new ArrayList<>(List.of("C#", "HTML"))
+        ));
+        ArrayList<Integer> results3 = new ArrayList<>(List.of(0, 1, 1, 1, 0, 1));
 
-        System.out.println(TournamentWinner.tournamentWinner(competitions, results));
+        System.out.println(TournamentWinner.tournamentWinner(competitions3, results3));
     }
 }
--- src/main/java/TournamentWinner.java
+++ src/main/java/TournamentWinner.java
@@ -1,7 +1,5 @@
 package main.java;
-
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TournamentWinner {
@@ -11,14 +9,14 @@ public class TournamentWinner {
                 .distinct()
                 .collect(Collectors.toMap(i->i, k-> 0));
 
-        LinkedHashMap<String, String> mathes = competitions.stream()
-                .collect(Collectors.toMap(ArrayList::getFirst, ArrayList::getLast, (x, y) -> y, LinkedHashMap::new));
+//        LinkedHashMap<String, String> mathes = competitions.stream()
+//                .collect(Collectors.toMap(ArrayList::getFirst, ArrayList::getLast, (x, y) -> x+"1", LinkedHashMap::new));
 
-        mathes.forEach((k, v) -> {
-            if(res.pollFirst() == 1){
-                teamsMap.replace(k, teamsMap.get(k)+1);
-            } else {teamsMap.replace(v, teamsMap.get(v)+1);}
-        });
+            for (int i = 0; i < competitions.size(); i++) {
+                if(res.pollFirst() == 1){
+                    teamsMap.replace(String.valueOf(competitions.get(i).getFirst()), teamsMap.get(competitions.get(i).getFirst())+1);
+                } else {teamsMap.replace(String.valueOf(competitions.get(i).getLast()), teamsMap.get(String.valueOf(competitions.get(i).getLast()))+1);}
+                }
 
         Optional<Integer> max = teamsMap.values().stream().max(Comparator.naturalOrder());
 
